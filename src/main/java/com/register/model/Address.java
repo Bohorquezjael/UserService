0f3b3abb9dbf7ef26d6610da04f66956db5700c8
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-05-03 17:32:44 -0600

    Agregar controlador, modelo y repositorio para reclutadores; incluir dependencia H2 y configuración de base de datos

--- src/main/java/com/register/model/Address.java
+++ src/main/java/com/register/model/Address.java
@@ -1,14 +1,16 @@
 package com.register.model;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Embeddable
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Address {
-    
+
     private String street;
     private String number;
     private String city;
